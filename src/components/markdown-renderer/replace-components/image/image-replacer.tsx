d9a040bdf3778d7d8aae102e7396fc926f64f79c
Tilman Vatteroth <git@tilmanvatteroth.de>
2021-07-18 22:30:27 +0200

    Use upload replacement frame
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

--- src/components/markdown-renderer/replace-components/image/image-replacer.tsx
+++ src/components/markdown-renderer/replace-components/image/image-replacer.tsx
@@ -9,11 +9,16 @@ import React from 'react'
 import { ComponentReplacer } from '../ComponentReplacer'
 import { ProxyImageFrame } from './proxy-image-frame'
 import { IframeSendingPlaceholderImageFrame } from './iframe-sending-placeholder-image-frame'
+import { uploadIdRegex, UploadIndicatingFrame } from './upload-indicating-frame'
 
 export type ImageClickHandler = (event: React.MouseEvent<HTMLImageElement, MouseEvent>) => void
 
 /**
- * Detects image tags and loads them via image proxy if configured.
+ * Detects image tags and either
+ * - loads them directly
+ * - loads them via image proxy if configured.
+ * - Shows an image placeholder frame if the url is "https://"
+ * - Shows an upload replacement frame if the url is "uploads-UPLOAD_ID"
  */
 export class ImageReplacer extends ComponentReplacer {
   private readonly clickHandler?: ImageClickHandler
@@ -36,18 +41,21 @@ export class ImageReplacer extends ComponentReplacer {
           height={node.attribs.height}
         />
       )
+    } else if (uploadIdRegex.test(node.attribs.src)) {
+      return <UploadIndicatingFrame width={node.attribs.width} height={node.attribs.height} />
+    } else {
+      return (
+        <ProxyImageFrame
+          id={node.attribs.id}
+          className={`${node.attribs.class} cursor-zoom-in`}
+          src={node.attribs.src}
+          alt={node.attribs.alt}
+          title={node.attribs.title}
+          width={node.attribs.width}
+          height={node.attribs.height}
+          onClick={this.clickHandler}
+        />
+      )
     }
-    return (
-      <ProxyImageFrame
-        id={node.attribs.id}
-        className={`${node.attribs.class} cursor-zoom-in`}
-        src={node.attribs.src}
-        alt={node.attribs.alt}
-        title={node.attribs.title}
-        width={node.attribs.width}
-        height={node.attribs.height}
-        onClick={this.clickHandler}
-      />
-    )
   }
 }
