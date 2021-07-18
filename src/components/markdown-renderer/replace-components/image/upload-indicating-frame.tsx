40d3e1a55cc2b1ab5acdba41a3dd8ee699db663c
Tilman Vatteroth <git@tilmanvatteroth.de>
2021-07-18 22:30:15 +0200

    Add replacement frame for uploads
    Signed-off-by: Tilman Vatteroth <git@tilmanvatteroth.de>

---
+++ src/components/markdown-renderer/replace-components/image/upload-indicating-frame.tsx
@@ -0,0 +1,33 @@
+/*
+ * SPDX-FileCopyrightText: 2021 The HedgeDoc developers (see AUTHORS file)
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import React from 'react'
+import { buildPlaceholderSizeCss } from './util'
+import { ForkAwesomeIcon } from '../../../common/fork-awesome/fork-awesome-icon'
+
+export interface PlaceholderImageFrameProps {
+  width?: string | number
+  height?: string | number
+}
+
+export const uploadIdRegex = /^upload-(.*)$/
+
+/**
+ * Shows a placeholder frame for images that are currently uploaded.
+ *
+ * @param width The frame width
+ * @param height The frame height
+ */
+export const UploadIndicatingFrame: React.FC<PlaceholderImageFrameProps> = ({ width, height }) => {
+  return (
+    <span
+      className='image-drop d-inline-flex flex-column align-items-center justify-content-center bg-primary text-white p-4'
+      style={buildPlaceholderSizeCss(width, height)}>
+      <h1 className={'border-bottom-0 my-2'}>Uploading image...</h1>
+      <ForkAwesomeIcon icon={'cog'} size={'5x'} fixedWidth={true} className='my-2 fa-spin' />
+    </span>
+  )
+}
